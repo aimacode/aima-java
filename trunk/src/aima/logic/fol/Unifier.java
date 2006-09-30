efa203b53976b03ecd50f7d062311cf503f2695c
aimajavaowner <aimajavaowner@69387497-ef19-0410-9dc2-eda6d4ab8473>
2006-09-30 11:56:38 +0000

    removed unnecessary println s in Unifier.java

--- trunk/src/aima/logic/fol/Unifier.java
+++ trunk/src/aima/logic/fol/Unifier.java
@@ -24,19 +24,15 @@ public class Unifier {
 		this.parser = parser;
 	}
 
-	//returns a Hashtable of rsults if succesful , null if fail
+	//returns a Hashtable of results if succesful , null if fail
 	public Hashtable unify(FOLNode x, FOLNode y, Hashtable theta) {
 
-		//System.out.println("UNIFY1 unify ( " + x + "," + y + "," + theta
-		//+ ") + xclass = " + x.getClass().getName());
+
 		if (theta == null) {
-			//System.out.println("theta is null\n");
 			return null;
 		} else if (x.equals(y)) {
-			//System.out.println(x + " = " + y);
 			return theta;
 		} else if (Variable.class.isInstance(x)) {
-			//System.out.println("x is a variable");
 			return unifyVar((Variable) x, y, theta);
 		} else if (Variable.class.isInstance(y)) {
 			return unifyVar((Variable) y, x, theta);
@@ -52,12 +48,9 @@ public class Unifier {
 	}
 
 	public Hashtable unify(List x, List y, Hashtable theta) {
-		//System.out.println("unifyList ( " + x + "," + y + "," + theta + ")");
 		if (theta == null) {
-			//System.out.println("unifyList returning null "+ theta + ")");
 			return null;
 		} else if (x.equals(y)) {
-			//System.out.println("unifyList returning "+ theta + ")");
 			return theta;
 		} else {
 			List restX = rest(x);
@@ -79,20 +72,15 @@ public class Unifier {
 	private FOLNode first(List x) {
 		List other = duplicate(x);
 		FOLNode first = (FOLNode) other.get(0);
-		//System.out.println("First(" + x + ") returns " + first);
 		return first;
 	}
 
 	private List rest(List x) {
 		if (x.size() == 1) {
-			//System.out.println("rest(" + x + ") returns [] ");
 			return new ArrayList();
 		} else {
 			List other = duplicate(x);
-			//System.out.println("REST - other before remove "+other);
 			other.remove(0);
-			//System.out.println("REST - other after remove "+other);
-			//System.out.println("rest(" + x + ") returns " + other);
 			return other;
 		}
 
@@ -116,25 +104,16 @@ public class Unifier {
 	}
 
 	private Hashtable unifyVar(Variable var, FOLNode x, Hashtable theta) {
-		//System.out.println("unifyVar ( " + var + "," + x + "," + theta +
-		// ")");
 		if (theta.keySet().contains(var)) {
-			//System.out.println("unifyVar #1 Theta" + theta + " contains " +
-			// var
-			//+ "( " + var + ") so returning -->");
+
 			return unify((FOLNode) theta.get(var), x, theta);
 		} else if (theta.keySet().contains(x)) {
-			//System.out.println("unifyVar #2 Theta" + theta + " contains " + x
-			//+ "( " + ") so returning -->");
+
 			return unify(var, (FOLNode) theta.get(var), theta);
 		} else if (occurCheck(var, x)) {
 			return null;//failure
 		} else {
-			//System.out.print("unify Var#4 added " + var + ":" + x + " to "
-			//+ theta + " giving ");
 			theta.put(var, x);
-			//System.out.println(theta);
-			//.print("returning "+theta);
 			return theta;
 		}
 	}
