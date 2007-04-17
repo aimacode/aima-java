91a1eba827596ddc5c55bede1b55894d4944d974
aimajavaowner <aimajavaowner@69387497-ef19-0410-9dc2-eda6d4ab8473>
2007-04-17 10:13:40 +0000

    added equals and hashcode to Pair.java

--- src/aima/util/Pair.java
+++ src/aima/util/Pair.java
@@ -1,20 +1,29 @@
-
 package aima.util;
 
-public class Pair<X, Y>  {
+public class Pair<X, Y> {
     private final X a;
+
     private final Y b;
 
     public Pair(X a, Y b) {
-        this.a = a;
-        this.b = b;
+	this.a = a;
+	this.b = b;
     }
 
     public X getFirst() {
-        return a;
+	return a;
     }
-    public Y getSecond() {
-        return b;
-    }
-}
 
+    public Y getSecond() {
+	return b;
+    }
+
+    public boolean equals(Pair<X, Y> p) {
+	return a.equals(p.a) && b.equals(p.b);
+    }
+
+    public int hashCode() {
+	return a.hashCode() + 31 * b.hashCode();
+    }
+
+}
