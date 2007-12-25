523c1125e766ccdb0d971e76400ce319ab68c9cc
magicindian <magicindian@69387497-ef19-0410-9dc2-eda6d4ab8473>
2007-12-25 15:31:25 +0000

    checking in search tests

--- src/aima/test/search/MisplacedTileHeuristicFunctionTest.java
+++ src/aima/test/search/MisplacedTileHeuristicFunctionTest.java
@@ -18,12 +18,12 @@ public class MisplacedTileHeuristicFunctionTest extends TestCase {
 		MisplacedTilleHeuristicFunction fn = new MisplacedTilleHeuristicFunction();
 		EightPuzzleBoard board = new EightPuzzleBoard(new int[] { 2, 0, 5, 6,
 				4, 8, 3, 7, 1 });
-		assertEquals(7, fn.getHeuristicValue(board));
+		assertEquals(7.0, fn.getHeuristicValue(board));
 
 		board = new EightPuzzleBoard(new int[] { 6, 2, 5, 3, 4, 8, 0, 7, 1 });
-		assertEquals(6, fn.getHeuristicValue(board));
+		assertEquals(6.0, fn.getHeuristicValue(board));
 
 		board = new EightPuzzleBoard(new int[] { 6, 2, 5, 3, 4, 8, 7, 0, 1 });
-		assertEquals(7, fn.getHeuristicValue(board));
+		assertEquals(7.0, fn.getHeuristicValue(board));
 	}
 }
