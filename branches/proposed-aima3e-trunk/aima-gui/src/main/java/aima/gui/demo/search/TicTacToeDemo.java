97b6d5c31da261be4cd3ee45ce161380b2797ccb
ctjoreilly <ctjoreilly@69387497-ef19-0410-9dc2-eda6d4ab8473>
2009-11-18 02:16:21 +0000

    - updated to conform to the changes committed to the aima-core project, SVN rev=308.

--- branches/proposed-aima3e-trunk/aima-gui/src/main/java/aima/gui/demo/search/TicTacToeDemo.java
+++ branches/proposed-aima3e-trunk/aima-gui/src/main/java/aima/gui/demo/search/TicTacToeDemo.java
@@ -1,8 +1,8 @@
 package aima.gui.demo.search;
 
-import aima.core.environment.games.GameState;
-import aima.core.environment.games.TicTacToe;
-import aima.core.environment.games.TicTacToeBoard;
+import aima.core.environment.tictactoe.TicTacToe;
+import aima.core.environment.tictactoe.TicTacToeBoard;
+import aima.core.search.adversarial.GameState;
 
 /**
  * @author Ravi Mohan
