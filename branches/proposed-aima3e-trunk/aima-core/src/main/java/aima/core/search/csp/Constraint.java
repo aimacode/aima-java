8a936daa29d88dca1259b49114c41019167b7e57
ctjoreilly <ctjoreilly@69387497-ef19-0410-9dc2-eda6d4ab8473>
2009-12-12 21:00:41 +0000

    - removing old branch

--- branches/proposed-aima3e-trunk/aima-core/src/main/java/aima/core/search/csp/Constraint.java
+++
@@ -1,9 +0,0 @@
-package aima.core.search.csp;
-
-/**
- * @author Ravi Mohan
- * 
- */
-public interface Constraint {
-	boolean isSatisfiedWith(Assignment assignment, String variable, Object value);
-}
