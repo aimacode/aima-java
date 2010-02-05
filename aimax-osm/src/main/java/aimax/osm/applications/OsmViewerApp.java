20cb7df369179452c47855be1f545f510c53d7a4
ctjoreilly <ctjoreilly@69387497-ef19-0410-9dc2-eda6d4ab8473>
2010-02-05 21:33:29 +0000

    - moving proposed aimax-osm project to trunk.

---
+++ aimax-osm/src/main/java/aimax/osm/applications/OsmViewerApp.java
@@ -0,0 +1,27 @@
+package aimax.osm.applications;
+
+import java.io.File;
+
+import aimax.osm.data.DataResource;
+import aimax.osm.reader.MapReader;
+import aimax.osm.viewer.MapViewFrame;
+
+/**
+ * Opens a <code>MapViewFrame</code> and initializes it with a
+ * map of Ulm, provided that a file with name ulm.osm is found
+ * in the resource/maps path.
+ * @author R. Lunde
+ */
+public class OsmViewerApp {
+	public static void main(String[] args) {
+		// indicates progress when reading large maps (for testing only)
+		// Logger.getLogger("aimax.osm").setLevel(Level.FINEST);
+		// Logger.getLogger("").getHandlers()[0].setLevel(Level.FINE);
+		
+		MapReader mapReader = new aimax.osm.reader.OsmReader();
+		MapViewFrame frame = new MapViewFrame(mapReader, DataResource.getULMFileResource());
+		frame.setTitle("OSM Viewer");
+		frame.setSize(800, 600);
+		frame.setVisible(true);
+	}
+}
