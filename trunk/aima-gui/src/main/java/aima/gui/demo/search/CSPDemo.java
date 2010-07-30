d3436718b960f8a2259e02eb534b41d4407627fa
Ruediger.Lunde <Ruediger.Lunde@69387497-ef19-0410-9dc2-eda6d4ab8473>
2010-07-30 20:33:00 +0000

    CSP package restructured.

--- trunk/aima-gui/src/main/java/aima/gui/demo/search/CSPDemo.java
+++ trunk/aima-gui/src/main/java/aima/gui/demo/search/CSPDemo.java
@@ -1,19 +1,21 @@
 package aima.gui.demo.search;
 
+import aima.core.search.csp.BacktrackingStrategy;
 import aima.core.search.csp.CSP;
 import aima.core.search.csp.MapCSP;
+import aima.core.search.csp.MinConflictsStrategy;
 
 /**
  * @author Ravi Mohan
- * 
+ * @author Ruediger Lunde
  */
 
 public class CSPDemo {
 	public static void main(String[] args) {
-		CSP csp = MapCSP.getMap();
+		CSP csp = new MapCSP();
 		System.out.println("Map Coloring - Backtracking ");
-		System.out.println(csp.backTrackingSearch());
+		System.out.println(new BacktrackingStrategy().solve(csp));
 		System.out.println("Map Coloring - Minimum Conflicts ");
-		System.out.println(csp.mcSearch(100));
+		System.out.println(new MinConflictsStrategy(100).solve(csp));
 	}
 }
