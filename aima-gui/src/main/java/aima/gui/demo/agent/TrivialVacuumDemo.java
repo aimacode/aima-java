f9bfbf7f382f87190f3d3e4d37b5b1a7241e0b0d
Ruediger.Lunde <Ruediger.Lunde@69387497-ef19-0410-9dc2-eda6d4ab8473>
2010-10-01 16:23:56 +0000

    - Documentation improved.

--- aima-gui/src/main/java/aima/gui/demo/agent/TrivialVacuumDemo.java
+++ aima-gui/src/main/java/aima/gui/demo/agent/TrivialVacuumDemo.java
@@ -1,20 +1,22 @@
 package aima.gui.demo.agent;
 
 import aima.core.agent.Agent;
+import aima.core.agent.Environment;
 import aima.core.agent.EnvironmentView;
 import aima.core.agent.impl.SimpleEnvironmentView;
 import aima.core.environment.vacuum.ModelBasedReflexVacuumAgent;
 import aima.core.environment.vacuum.VacuumEnvironment;
 
 /**
- * Demonstrates the use of the VaccumEnvironment and different vacuum agents.
+ * Demonstrates, how to set up a simple environment, place an agent in it,
+ * and run it. The vacuum world is used as a simple example.
  * 
  * @author Ruediger Lunde
  */
 public class TrivialVacuumDemo {
 	public static void main(String[] args) {
 		// create environment with random state of cleaning.
-		VacuumEnvironment env = new VacuumEnvironment();
+		Environment env = new VacuumEnvironment();
 		EnvironmentView view = new SimpleEnvironmentView();
 		env.addEnvironmentView(view);
 		
