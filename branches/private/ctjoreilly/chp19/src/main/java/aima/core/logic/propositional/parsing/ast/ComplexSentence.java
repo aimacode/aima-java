9b6a60fa4c0fdff8c2e82a41dbf2d839ba4e3d4f
ctjoreilly <ctjoreilly@69387497-ef19-0410-9dc2-eda6d4ab8473>
2010-12-08 20:38:41 +0000

    - removing unused branch.

--- branches/private/ctjoreilly/chp19/src/main/java/aima/core/logic/propositional/parsing/ast/ComplexSentence.java
+++
@@ -1,9 +0,0 @@
-package aima.core.logic.propositional.parsing.ast;
-
-/**
- * @author Ravi Mohan
- * 
- */
-public abstract class ComplexSentence extends Sentence {
-
-}
--- branches/private/ctjoreilly/chp19/src/main/java/aima/core/probability/Randomizer.java
+++
@@ -1,9 +0,0 @@
-package aima.core.probability;
-
-/**
- * @author Ravi Mohan
- * 
- */
-public interface Randomizer {
-	public double nextDouble();
-}
