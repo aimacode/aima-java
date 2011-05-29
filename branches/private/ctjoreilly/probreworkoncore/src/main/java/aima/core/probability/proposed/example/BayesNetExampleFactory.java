d189f64014fc7040e9583e04b906d0d05f2fcda1
ctjoreilly <ctjoreilly@69387497-ef19-0410-9dc2-eda6d4ab8473>
2011-05-29 23:20:03 +0000

    - suppressed unwanted warnings.

--- branches/private/ctjoreilly/probreworkoncore/src/main/java/aima/core/probability/proposed/example/BayesNetExampleFactory.java
+++ branches/private/ctjoreilly/probreworkoncore/src/main/java/aima/core/probability/proposed/example/BayesNetExampleFactory.java
@@ -5,29 +5,35 @@ import aima.core.probability.proposed.bayes.FiniteNode;
 
 public class BayesNetExampleFactory {
 	public static BayesianNetwork construct2FairDiceNetwor() {
-		FiniteNode dice1 = new FiniteNode(ExampleRV.DICE_1_RV, new double[] { 1.0 / 6.0,
-				1.0 / 6.0, 1.0 / 6.0, 1.0 / 6.0, 1.0 / 6.0, 1.0 / 6.0 });
-		FiniteNode dice2 = new FiniteNode(ExampleRV.DICE_2_RV, new double[] { 1.0 / 6.0,
-				1.0 / 6.0, 1.0 / 6.0, 1.0 / 6.0, 1.0 / 6.0, 1.0 / 6.0 });
+		FiniteNode dice1 = new FiniteNode(ExampleRV.DICE_1_RV, new double[] {
+				1.0 / 6.0, 1.0 / 6.0, 1.0 / 6.0, 1.0 / 6.0, 1.0 / 6.0,
+				1.0 / 6.0 });
+		FiniteNode dice2 = new FiniteNode(ExampleRV.DICE_2_RV, new double[] {
+				1.0 / 6.0, 1.0 / 6.0, 1.0 / 6.0, 1.0 / 6.0, 1.0 / 6.0,
+				1.0 / 6.0 });
 
 		return new BayesianNetwork(dice1, dice2);
 	}
 
 	public static BayesianNetwork constructToothacheCavityCatchNetwork() {
-		FiniteNode cavity = new FiniteNode(ExampleRV.CAVITY_RV, new double[] { 0.2, 0.8 });
-		FiniteNode toothache = new FiniteNode(ExampleRV.TOOTHACHE_RV, new double[] {
-		// C=true, T=true
-				0.6,
-				// C=true, T=false
-				0.4,
-				// C=false, T=true
-				0.1,
-				// C=false, T=false
-				0.9
+		FiniteNode cavity = new FiniteNode(ExampleRV.CAVITY_RV, new double[] {
+				0.2, 0.8 });
+		@SuppressWarnings("unused")
+		FiniteNode toothache = new FiniteNode(ExampleRV.TOOTHACHE_RV,
+				new double[] {
+						// C=true, T=true
+						0.6,
+						// C=true, T=false
+						0.4,
+						// C=false, T=true
+						0.1,
+						// C=false, T=false
+						0.9
 
-		}, cavity);
+				}, cavity);
+		@SuppressWarnings("unused")
 		FiniteNode catchN = new FiniteNode(ExampleRV.CATCH_RV, new double[] {
-		// C=true, Catch=true
+				// C=true, Catch=true
 				0.9,
 				// C=true, Catch=false
 				0.1,
@@ -40,20 +46,24 @@ public class BayesNetExampleFactory {
 	}
 
 	public static BayesianNetwork constructToothacheCavityCatchWeatherNetwork() {
-		FiniteNode cavity = new FiniteNode(ExampleRV.CAVITY_RV, new double[] { 0.2, 0.8 });
-		FiniteNode toothache = new FiniteNode(ExampleRV.TOOTHACHE_RV, new double[] {
-		// C=true, T=true
-				0.6,
-				// C=true, T=false
-				0.4,
-				// C=false, T=true
-				0.1,
-				// C=false, T=false
-				0.9
+		FiniteNode cavity = new FiniteNode(ExampleRV.CAVITY_RV, new double[] {
+				0.2, 0.8 });
+		@SuppressWarnings("unused")
+		FiniteNode toothache = new FiniteNode(ExampleRV.TOOTHACHE_RV,
+				new double[] {
+						// C=true, T=true
+						0.6,
+						// C=true, T=false
+						0.4,
+						// C=false, T=true
+						0.1,
+						// C=false, T=false
+						0.9
 
-		}, cavity);
+				}, cavity);
+		@SuppressWarnings("unused")
 		FiniteNode catchN = new FiniteNode(ExampleRV.CATCH_RV, new double[] {
-		// C=true, Catch=true
+				// C=true, Catch=true
 				0.9,
 				// C=true, Catch=false
 				0.1,
@@ -62,7 +72,7 @@ public class BayesNetExampleFactory {
 				// C=false, Catch=false
 				0.8 }, cavity);
 		FiniteNode weather = new FiniteNode(ExampleRV.WEATHER_RV, new double[] {
-		// sunny
+				// sunny
 				0.6,
 				// rain
 				0.1,
@@ -75,29 +85,31 @@ public class BayesNetExampleFactory {
 	}
 
 	public static BayesianNetwork constructMeningitisStiffNeckNetwork() {
-		FiniteNode meningitis = new FiniteNode(ExampleRV.MENINGITIS_RV, new double[] {
-				1.0 / 50000.0, 1.0 - (1.0 / 50000.0) });
-		FiniteNode stiffneck = new FiniteNode(ExampleRV.STIFF_NECK_RV, new double[] {
-		// M=true, S=true
-				0.7,
-				// M=true, S=false
-				0.3,
-				// M=false, S=true
-				0.009986199723994478,
-				// M=false, S=false
-				0.9900138002760055
+		FiniteNode meningitis = new FiniteNode(ExampleRV.MENINGITIS_RV,
+				new double[] { 1.0 / 50000.0, 1.0 - (1.0 / 50000.0) });
+		@SuppressWarnings("unused")
+		FiniteNode stiffneck = new FiniteNode(ExampleRV.STIFF_NECK_RV,
+				new double[] {
+						// M=true, S=true
+						0.7,
+						// M=true, S=false
+						0.3,
+						// M=false, S=true
+						0.009986199723994478,
+						// M=false, S=false
+						0.9900138002760055
 
-		}, meningitis);
+				}, meningitis);
 		return new BayesianNetwork(meningitis);
 	}
 
 	public static BayesianNetwork constructBurglaryAlarmNetwork() {
-		FiniteNode burglary = new FiniteNode(ExampleRV.BURGLARY_RV, new double[] { 0.001,
-				0.999 });
-		FiniteNode earthquake = new FiniteNode(ExampleRV.EARTHQUAKE_RV, new double[] {
-				0.002, 0.998 });
+		FiniteNode burglary = new FiniteNode(ExampleRV.BURGLARY_RV,
+				new double[] { 0.001, 0.999 });
+		FiniteNode earthquake = new FiniteNode(ExampleRV.EARTHQUAKE_RV,
+				new double[] { 0.002, 0.998 });
 		FiniteNode alarm = new FiniteNode(ExampleRV.ALARM_RV, new double[] {
-		// B=true, E=true, A=true
+				// B=true, E=true, A=true
 				0.95,
 				// B=true, E=true, A=false
 				0.05,
@@ -113,24 +125,28 @@ public class BayesNetExampleFactory {
 				0.001,
 				// B=false, E=false, A=false
 				0.999 }, burglary, earthquake);
-		FiniteNode johnCalls = new FiniteNode(ExampleRV.JOHN_CALLS_RV, new double[] {
-		// A=true, J=true
-				0.90,
-				// A=true, J=false
-				0.10,
-				// A=false, J=true
-				0.05,
-				// A=false, J=false
-				0.95 }, alarm);
-		FiniteNode maryCalls = new FiniteNode(ExampleRV.MARY_CALLS_RV, new double[] {
-		// A=true, M=true
-				0.70,
-				// A=true, M=false
-				0.30,
-				// A=false, M=true
-				0.01,
-				// A=false, M=false
-				0.99 }, alarm);
+		@SuppressWarnings("unused")
+		FiniteNode johnCalls = new FiniteNode(ExampleRV.JOHN_CALLS_RV,
+				new double[] {
+						// A=true, J=true
+						0.90,
+						// A=true, J=false
+						0.10,
+						// A=false, J=true
+						0.05,
+						// A=false, J=false
+						0.95 }, alarm);
+		@SuppressWarnings("unused")
+		FiniteNode maryCalls = new FiniteNode(ExampleRV.MARY_CALLS_RV,
+				new double[] {
+						// A=true, M=true
+						0.70,
+						// A=true, M=false
+						0.30,
+						// A=false, M=true
+						0.01,
+						// A=false, M=false
+						0.99 }, alarm);
 
 		return new BayesianNetwork(burglary, earthquake);
 	}
