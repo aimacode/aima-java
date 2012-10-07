8a942683f2d097b0e30cbcfbbd19e668e9d093ec
ctjoreilly <ctjoreilly@69387497-ef19-0410-9dc2-eda6d4ab8473>
2012-10-07 20:12:54 +0000

    - removing branch that has been integrated back into the trunk.

--- branches/issue24/aimax-osm/src/main/java/aimax/osm/routing/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package contains useful helper classes for applications which apply
- * search algorithms from the aima-core library to solve route finding
- * problems based on Open Street Map data. 
- */
-package aimax.osm.routing;
