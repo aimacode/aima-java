e1c95cdbf7731edb7466c47316c98c7c5238a818
ctjoreilly <ctjoreilly@69387497-ef19-0410-9dc2-eda6d4ab8473>
2015-02-09 04:08:18 +0000

    Add placeholder test for basic implementations of TABLE-DRIVE-AGENT and MODEL-BASED-REFLEX-AGENT.

---
+++ branches/AIMA4e/core/src/test/java/aima/test/core/unit/agent/BasicTableDrivenAgentTest.java
@@ -0,0 +1,15 @@
+package aima.test.core.unit.agent;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Ciaran O'Reilly
+ */
+public class BasicTableDrivenAgentTest {
+
+    @Test
+    public void testTODO() {
+        Assert.fail("TODO");
+    }
+}
