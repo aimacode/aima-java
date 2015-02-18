ba74d0478b8857354a5882e4757e0e83442f6330
ctjoreilly <ctjoreilly@69387497-ef19-0410-9dc2-eda6d4ab8473>
2015-02-18 03:24:01 +0000

    Add basic implementation of Depth-First-Search.

---
+++ core/src/main/java/aima/core/search/uninformed/BasicDepthFirstSearch.java
@@ -0,0 +1,23 @@
+package aima.core.search.uninformed;
+
+import aima.core.api.search.Node;
+
+import java.util.Collections;
+import java.util.Queue;
+
+/**
+ * Artificial Intelligence A Modern Approach (4th Edition): page ??.<br>
+ * <br>
+ * The depth-first algorithm is an instance of the graph-search algorithm
+ * in Figure ??; whereas breadth-first-search uses a FIFO queue, depth-first
+ * search uses a LIFO queue.
+ *
+ * @author Ciaran O'Reilly
+ */
+public class BasicDepthFirstSearch<S> extends BasicGraphSearch<S> {
+
+    @Override
+    public Queue<Node<S>> newFrontier() {
+        return Collections.asLifoQueue(new FrontierQueue());
+    }
+}
