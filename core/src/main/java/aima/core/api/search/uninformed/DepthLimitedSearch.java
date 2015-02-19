72e5e87649a666892fd22000cea8ceeb6d89b319
ctjoreilly <ctjoreilly@69387497-ef19-0410-9dc2-eda6d4ab8473>
2015-02-19 03:46:11 +0000

    Add DEPTH-LIMITED-SEARCH pseudo-code implementation. Add corresponding basic implementation.

---
+++ core/src/main/java/aima/core/api/search/uninformed/DepthLimitedSearch.java
@@ -0,0 +1,78 @@
+package aima.core.api.search.uninformed;
+
+import aima.core.api.agent.Action;
+import aima.core.api.search.Node;
+import aima.core.api.search.Problem;
+
+import java.util.List;
+
+/**
+ * Artificial Intelligence A Modern Approach (4th Edition): Figure ??, page ??.<br>
+ * <br>
+ *
+ * <pre>
+ * function DEPTH-LIMITED-SEARCH(problem, limit) returns a solution, or failure/cutoff
+ *   return RECURSIVE-DLS(MAKE-NODE(problem.INITIAL-STATE), problem, limit)
+ *
+ * function RECURSIVE-DLS(node, problem, limit) returns a solution, or failure/cutoff
+ *   if problem.GOAL-TEST(node.STATE) then return SOLUTION(node)
+ *   else if limit = 0 then return cutoff
+ *   else
+ *       cutoff_occurred? &lt;- false
+ *       for each action in problem.ACTIONS(node.STATE) do
+ *           child &lt;- CHILD-NODE(problem, node, action)
+ *           result &lt;- RECURSIVE-DLS(child, problem, limit - 1)
+ *           if result = cutoff then cutoff_occurred? &lt;- true
+ *           else if result != failure then return result
+ *       if cutoff_occurred? then return cutoff else return failure
+ * </pre>
+ *
+ * Figure ?? A recursive implementation of depth-limited search.
+ *
+ * @author Ciaran O'Reilly
+ */
+public interface DepthLimitedSearch<S> extends SearchFunction<S> {
+
+    // function DEPTH-LIMITED-SEARCH(problem, limit) returns a solution, or failure/cutoff
+    default List<Action> depthLimitedSearch(Problem<S> problem, int limit) {
+        // return RECURSIVE-DLS(MAKE-NODE(problem.INITIAL-STATE), problem, limit)
+        return recursiveDLS(newNode(problem.initialState()), problem, limit);
+    }
+
+    // function RECURSIVE-DLS(node, problem, limit) returns a solution, or failure/cutoff
+    default List<Action> recursiveDLS(Node<S> node, Problem<S> problem, int limit) {
+        // if problem.GOAL-TEST(node.STATE) then return SOLUTION(node)
+        if (problem.isGoalState(node.state())) {
+            return solution(node);
+        } // else if limit = 0 then return cutoff
+        else if (limit == 0) {
+            return cutoff();
+        } // else
+        else {
+            // cutoff_occurred? <- false
+            boolean cutoff_occurred = false;
+            // for each action in problem.ACTIONS(node.STATE) do
+            for (Action action : problem.actions(node.state())) {
+                // child <- CHILD-NODE(problem, node, action)
+                Node<S> child = childNode(problem, node, action);
+                // result <- RECURSIVE-DLS(child, problem, limit - 1)
+                List<Action> result = recursiveDLS(child, problem, limit-1);
+                // if result = cutoff then cutoff_occurred? <- true
+                if (result == cutoff()) {
+                    cutoff_occurred = true;
+                } // else if result != failure then return result
+                else if (result != failure()) { return result; }
+            }
+            // if cutoff_occurred? then return cutoff else return failure
+            if (cutoff_occurred) { return cutoff(); } else { return failure(); }
+        }
+    }
+
+    @Override
+    default List<Action> apply(Problem<S> problem) {
+        return depthLimitedSearch(problem, limit());
+    }
+
+    int limit();
+    List<Action> cutoff();
+}
