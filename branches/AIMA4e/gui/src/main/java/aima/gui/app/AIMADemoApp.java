a57d2398330060eb41331883ff2cae086d72c35b
ctjoreilly <ctjoreilly@69387497-ef19-0410-9dc2-eda6d4ab8473>
2015-02-28 18:01:19 +0000

    Add skeleton for demo gui.

--- branches/AIMA4e/gui/src/main/java/aima/gui/app/AIMADemoApp.java
+++ branches/AIMA4e/gui/src/main/java/aima/gui/app/AIMADemoApp.java
@@ -1,7 +1,26 @@
 package aima.gui.app;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * @author Ciaran O'Reilly
  */
-public class AIMADemoApp {
+public class AIMADemoApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("aimademoapp.fxml"));
+        primaryStage.setTitle("AIMA4e-Java Demos");
+        primaryStage.setScene(new Scene(root, 1024, 768));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
