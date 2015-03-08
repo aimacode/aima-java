89699021361125f6191ac50becd45e026ec95998
ctjoreilly <ctjoreilly@69387497-ef19-0410-9dc2-eda6d4ab8473>
2015-03-08 22:53:32 +0000

    Additional minor work on GUI.

--- branches/AIMA4e/gui/src/main/java/aima/gui/demo/search/tree/TreeSearchController.java
+++ branches/AIMA4e/gui/src/main/java/aima/gui/demo/search/tree/TreeSearchController.java
@@ -3,7 +3,9 @@ package aima.gui.demo.search.tree;
 import aima.gui.demo.search.problem.RectangularGridProblemController;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
@@ -12,13 +14,17 @@ import java.io.IOException;
  * @author Ciaran O'Reilly
  */
 public class TreeSearchController {
-    @FXML private BorderPane problemPane;
+    @FXML private AnchorPane problemPane;
     @FXML private BorderPane searchAlgoPane;
     @FXML private BorderPane searchInfoPane;
 
     @FXML
     private void initialize() throws IOException {
         Pane problem = FXMLLoader.load(RectangularGridProblemController.class.getResource("rectangulargridproblem.fxml"));
-        problemPane.setCenter(problem);
+        AnchorPane.setTopAnchor(problem, 0.0);
+        AnchorPane.setLeftAnchor(problem, 0.0);
+        AnchorPane.setRightAnchor(problem, 0.0);
+        AnchorPane.setBottomAnchor(problem, 0.0);
+        problemPane.getChildren().addAll(problem);
     }
 }
