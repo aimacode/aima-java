af131e657936272a39aa82e69e240f18981ff68d
Shubhankar Mohapatra <mshubhankar@yahoo.co.in>
2016-03-24 00:18:08 +0530

    Create Variable.java

---
+++ core/src/main/java/aima/core/search/csp/Variable.java
@@ -0,0 +1,36 @@
+package aima.core.search.csp;
+
+/**
+ * A variable is a distinguishable object with a name.
+ * 
+ * @author Shubhankar Mohapatra
+ */
+public class Variable {
+	private String name;
+
+	public Variable(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String toString() {
+		return name;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
+		if (obj.getClass() == getClass())
+			return this.name.equals(((Variable) obj).name);
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return name.hashCode();
+	}
+}
