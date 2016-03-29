4c72febdcf4e3dca6f7f0ef2d0eb92bcfbd16154
subham mishra <smsubham342@gmail.com>
2016-03-30 00:55:47 +0530

    Create ActionsFunction.java

---
+++ core/src/main/java/aima/core/search/framework/ActionsFunction.java
@@ -0,0 +1,24 @@
+package aima.core.search.framework;
+
+import java.util.Set;
+
+/**
+ * Artificial Intelligence A Modern Approach (3rd Edition): page 67.<br>
+ * <br>
+ * Given a particular state s, ACTIONS(s) returns the set of actions that can be
+ * executed in s. We say that each of these actions is <b>applicable</b> in s.
+ * 
+ * @author Ciaran O'Reilly
+ * 
+ */
+public interface ActionsFunction<A> {
+	/**
+	 * Given a particular state s, returns the set of actions that can be
+	 * executed in s.
+	 * 
+	 * @param s
+	 *            a particular state.
+	 * @return the set of actions that can be executed in s.
+	 */
+	Set<A> actions(Object s);
+}
