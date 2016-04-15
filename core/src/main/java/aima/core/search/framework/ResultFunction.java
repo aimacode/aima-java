7a7fa9528d740fd4d1ab6b9c2dfe7938f9696856
subham mishra <smsubham342@gmail.com>
2016-04-16 02:28:26 +0530

    Create ResultFunction.java

---
+++ core/src/main/java/aima/core/search/framework/ResultFunction.java
@@ -0,0 +1,28 @@
+package aima.core.search.framework;
+
+import java.util.Set;
+
+/**
+ * Artificial Intelligence A Modern Approach (4th Edition): page ??. <br>
+ * <br>
+ * Closely related to ResultFunction, but for non-deterministic problems; in
+ * these problems, the outcome of an action will be a set of results, not a
+ * single result. This class implements the functionality of RESULTS(s, a), page
+ * 136, returning the states resulting from doing action a in state s.
+ * 
+ * @author Subham Mishra
+ * @author Andrew Brown
+ */
+public interface ResultFunction<A> {
+
+	/**
+	 * Returns the states that result from doing action a in state s
+	 * 
+	 * @param s
+	 *            a particular state.
+	 * @param a
+	 *            an action to be performed in state s.
+	 * @return the states that result from doing action a in state s.
+	 */
+	Set<Object> result(Object s, A a);
+}
