ac2e92764b7abccd548bc99139de4eac2d7e0cf3
subham mishra <smsubham342@gmail.com>
2016-04-16 02:04:10 +0530

    Update GRAPHPLAN.java
    partial implementation

--- aima-core/src/main/java/aima/core/search/planning/GRAPHPLAN.java
+++ aima-core/src/main/java/aima/core/search/planning/GRAPHPLAN.java
@@ -3,12 +3,22 @@ package aima.core.search.planning;
 import java.util.List;
 import java.util.Hashtable;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.Iterator;
 
-import aima.core.search.framework.Problem;
+import aima.core.search.framework.Problem_Extend;
 import aima.core.agent.Action;
+import aima.core.agent.State;
+import aima.core.agent.impl.aprog.simplerule.*;
+import aima.core.search.framework.*;
 
 /**
  * <h2> GRAPHPLAN.java
+ * Note- This is partial implementation andcomplete implementation will be added soon
  * </h2> 
  * 
  * Artificial Intelligence A Modern Approach (3rd Edition): page 383 Figure 10.9.<br>
@@ -16,16 +26,16 @@ import aima.core.agent.Action;
  * 
  * <pre>
  * <code>
- *function GRAPHPLAN(problem) returns solution or failure
- *graph <- INITIAL-PLANNING-GRAPH(problem)
- *goals <- CONJUNCTS(problem.GOAL)
+ *function GRAPHPLAN(Problem_Extend) returns solution or failure
+ *graph <- INITIAL-PLANNING-GRAPH(Problem_Extend)
+ *goals <- CONJUNCTS(Problem_Extend.GOAL)
  *nogoods <- an empty hash table
- *for tl = 0 to ∞ do
+ *for tl = 0 to Inf do
  *if goals all non-mutex in St of graph then
  *solution <- EXTRACT-SOLUTION(graph, goals, NUMLEVELS(graph), nogoods)
- *if solution 6= failure then return solution
+ *if solution != failure then return solution
  *if graph and nogoods have both leveled off then return failure
- *graph <- EXPAND-GRAPH(graph, problem)
+ *graph <- EXPAND-GRAPH(graph, Problem_Extend)
  * </code>
  * </pre>
  * Figure 10.9 The GRAPHPLAN algorithm. GRAPHPLAN calls EXPAND-GRAPH to add a
@@ -38,26 +48,26 @@ import aima.core.agent.Action;
     {
 	   protected Hashtable<State,Action> solution=new Hashtable<State,Action>(); 
 	   /**
-	    * function GRAPHPLAN( problem) returns
+	    * function GRAPHPLAN( Problem_Extend) returns
 	    * solution or failure
 	    * 
 	    * @param p
-	    *       Problem whose solution is to be found
+	    *       Problem_Extend whose solution is to be found
 	    * @return
 	    * 
 	    */
-	public Hashtable<State,Action> graphplan(Problem p)
+	public Hashtable<State,Action> graphplan(Problem_Extend p)
 	{
-		List<Action> graph = new ArrayList<Action>();
-		//graph <- INITIAL-PLANNING-GRAPH(problem)
+		Hashtable<State,Iterator<Action>> graph = new Hashtable<State,Iterator<Action>>();
+		//graph <- INITIAL-PLANNING-GRAPH(Problem_Extend)
 		graph = INITIALPLANNINGGRAPH(p); 
 	    List<Action> goals = new ArrayList<Action>();
-       //goals <- CONJUNCTS(problem.GOAL)
+       //goals <- CONJUNCTS(Problem_Extend.GOAL)
 		goals=CONJUNCTS(p);
 	    Hashtable<Integer,String> nogoods=new Hashtable<Integer,String>();  //an empty hash table
 		for(int tl=0;tl<1/0.;tl++) //1/0. = Infinity
 		{
-			if(isNonMutex()) //if goals all non-mutex in St of graph
+			if(isNonMutex(graph,p)) //if goals all non-mutex in St of graph
 			{
 				solution=EXTRACTSOLUTION(graph,goals,0,nogoods);
 				if(solution!=null); //null represents failure
@@ -70,47 +80,75 @@ import aima.core.agent.Action;
 		
 		return null; //returns failure
 	}
-	//
-	//SUPPORTING CODE
-	//
-	interface State {
-	
-}
 	/**
 	 * used to initialize the planning graph 
 	 * to a one-level (So) graph representing the initial state
 	 * @param pb
-	 *      Takes problem
+	 *      Takes Problem_Extend
 	 * @return
 	 *       initial state So
 	 */
-	public abstract List<Action> INITIALPLANNINGGRAPH(Problem pb);
+	public Hashtable<State, Iterator<Action>> INITIALPLANNINGGRAPH(Problem_Extend pb)
+	{
+		return pb.getInitialState();
+	}
 	/**
-	 * Used to extract Goal Conditions form Problem
+	 * Used to extract Goal Conditions form Problem_Extend
 	 * @param pb
-	 *      Takes Problem
+	 *      Takes Problem_Extend
 	 * @return
-	 *      Goals in Problem pb
+	 *      Goals in Problem_Extend pb
 	 */
-	public abstract List<Action>  CONJUNCTS(Problem pb);
+	public List<Action>  CONJUNCTS(Problem_Extend pb)
+	{
+		return pb.Goal;
+	}
 	/**
 	 * Expands graph to add actions whose preconditions 
 	 * are satisfied by So
 	 * 
-	 * @param g
+	 * @param graph
 	 *     accepts graph with states
 	 * @param pb
-	 *     Takes Problem 
+	 *     Takes Problem_Extend 
 	 * 
 	 * @return
 	 *     Expanded graph
 	 * 
 	 */
-	public abstract List<Action> EXPANDGRAPH(List<Action> g,Problem pb);
+	public Hashtable<State, Iterator<Action>> EXPANDGRAPH(Hashtable<State, Iterator<Action>> graph,Problem_Extend pb)
+	{
+		//yet to be implemented completely
+			Set<Iterator<State>> a=pb.action_Conditions.keySet(); //State
+			Enumeration<Action> e=pb.action_Conditions.elements(); //Actions
+			while(e.hasMoreElements())
+			{
+				for(int i=0;;i++)
+				{
+					for (State key : graph.keySet())
+					{
+						
+					}
+						
+				}
+			}
+			
+			
+			
+			//for (State key1 : a.keySet())
+			
+			{
+				//for (State key2 : a.key1.keySet())
+				{
+					
+				}
+			}
+		}
+	}
 	/**
 	 * Once all goal states are in Si it is called to find solution
 	 * 
-	 * @param g
+	 * @param graph
 	 *      graph
 	 * @param goal
 	 *   list of goal state
@@ -124,16 +162,33 @@ import aima.core.agent.Action;
 	 *  if solution exists it is returned else
 	 *   null , indicating failure is returned
 	 */
-	public abstract Hashtable<State,Action> EXTRACTSOLUTION(List<Action> g,List<Action> goal,int i,Hashtable<Integer,String> nogood);
+	public Hashtable<State,Action> EXTRACTSOLUTION(Hashtable<State, Set<Action>> graph,List<Action> goal,int i,Hashtable<Integer,String> nogood)
+	{
+	//yet to be implemented	
+	}
 	/**
 	 * to check if all states in goals are non-mutex
+	 * will return true if goal state is already in 
+	 * graph , false otherwise
 	 * 
 	 * @return
 	 * true if all the literals from the goal are present
      * in S2, and none of them is mutex with any other
      * false otherwise
 	 */
-	public abstract boolean isNonMutex();
+	public boolean isNonMutex(Hashtable<State,Action> graph,Problem_Extend pb)
+	{
+		
+		/**Enumeration e = graph.keys();
+		for (State key : graph.keySet())
+		{
+			if(pb.isGoalState(key));
+			return true;
+		}
+		return false;
+		**/
+		return pb.isGoalState(graph);
+	}
 	/**
 	 * check if graph and nogoods have both leveled off
 	 * 
@@ -142,7 +197,10 @@ import aima.core.agent.Action;
 	 * false otherwise
 	 * 
 	 */
-	public abstract boolean isLeveledOff();
+	public boolean isLeveledOff()
+	{
+		//yet to be implemented
+	}
 	/**
 	 * used to find numlevel of action g
 	 * 
@@ -151,5 +209,8 @@ import aima.core.agent.Action;
 	 * @return
 	 *  numlevel of action g
 	 */
-	public abstract int NUMLEVELS(List<Action> g);
+	public int NUMLEVELS(List<Action> g)
+	{
+		//yet to be implemented
+	}
 }
