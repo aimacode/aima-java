bd67a3952aafd8d47c3973b541e8f1bee36f9b73
ruediger.lunde@gmail.com <alinghi.14>
2016-04-17 13:59:54 +0200

    Documentation improved.

--- aima-core/src/main/java/aima/core/search/framework/package-info.java
+++ aima-core/src/main/java/aima/core/search/framework/package-info.java
@@ -1,17 +1,18 @@
 /**
  * This package contains base classes for search algorithm implementations. Many
- * algorithms are basically queue-based algorithms. They construct a tree of
- * nodes which represents the possible sequences of actions and the
+ * search algorithms are basically queue-based algorithms. They construct a tree
+ * of nodes which represents the possible sequences of actions and the
  * corresponding resulting states. A queue is used to manage and prioritize the
- * current end points of already analyzed sequences of actions.
+ * current end points of already analyzed sequences.
  * 
  * <br>
  * Specializations are possible in two ways: There are different ways to define
  * a queue (A), and to use the queue to explore the search space (B). (A) is
  * about prioritizing nodes, which can be done by time (e.g. first come first
  * serve), by comparator, or by evaluation function. (B) is about controlling
- * the simulated exploration based on a given queue data structure. It includes
- * strategies for filtering nodes to avoid getting stuck in loops.
+ * the simulated exploration of the search space based on a given queue data
+ * structure. This includes strategies for filtering nodes to avoid getting
+ * stuck in loops.
  * 
  * <br>
  * To support arbitrary combinations of different strategies for (A) and (B),
