ae5ef81f14d95850e2b2590263563a07c0c13752
subham mishra <smsubham342@gmail.com>
2016-04-20 09:44:26 +0530

    Create EnvironmentState.java

---
+++ core/src/main/java/aima/core/agent/EnvironmentState.java
@@ -0,0 +1,10 @@
+package aima.core.agent;
+
+/**
+ * An interface used to indicate a possible state of an Environment.
+ * 
+ * @author Ciaran O'Reilly
+ */
+public interface EnvironmentState {
+
+}
