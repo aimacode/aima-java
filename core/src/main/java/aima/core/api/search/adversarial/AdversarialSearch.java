6ee8b1989ebd7251c5b13b30e037d8977738e879
Anurag Rai <7anuragrai7@gmail.com>
2016-04-20 19:54:38 +0530

    Created AdversarialSearch.java

---
+++ core/src/main/java/aima/core/api/search/adversarial/AdversarialSearch.java
@@ -0,0 +1,12 @@
+package aima.core.api.search.adversarial;
+
+import java.util.function.Function;
+
+/**
+ * 
+ * @author Anurag Rai
+ *
+ */
+public interface AdversarialSearch<A,S> extends Function<S,A> {
+
+}
