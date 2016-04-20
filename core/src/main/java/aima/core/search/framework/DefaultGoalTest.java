8ad2ca664d79c18a751037fd095a5f8785c3419b
subham mishra <smsubham342@gmail.com>
2016-04-20 10:06:51 +0530

    Create DefaultGoalTest.java

---
+++ core/src/main/java/aima/core/search/framework/DefaultGoalTest.java
@@ -0,0 +1,18 @@
+package aima.core.search.framework;
+
+/**
+ * Checks whether a given state equals an explicitly specified goal state.
+ * 
+ * @author Ruediger Lunde
+ */
+public class DefaultGoalTest implements GoalTest {
+	private Object goalState;
+
+	public DefaultGoalTest(Object goalState) {
+		this.goalState = goalState;
+	}
+
+	public boolean isGoalState(Object state) {
+		return goalState.equals(state);
+	}
+}
