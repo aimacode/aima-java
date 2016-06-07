ab52e4bf4d872816bdb88ca923e73abdc1c2fc79
Ciaran O'Reilly <ctjoreilly@gmail.com>
2016-06-07 08:42:39 -0700

    Additional minor formatting.

--- core/src/main/java/aima/core/search/basic/TreeSearch.java
+++ core/src/main/java/aima/core/search/basic/TreeSearch.java
@@ -1,6 +1,5 @@
 package aima.core.search.basic;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -15,7 +14,8 @@ import aima.core.search.basic.support.BasicNodeFactory;
 import aima.core.search.basic.support.BasicSearchController;
 
 /**
- * Artificial Intelligence A Modern Approach (4th Edition): Figure ??, page ??.<br>
+ * Artificial Intelligence A Modern Approach (4th Edition): Figure ??, page ??.
+ * <br>
  * <br>
  *
  * <pre>
@@ -32,59 +32,55 @@ import aima.core.search.basic.support.BasicSearchController;
  *
  * @author Ciaran O'Reilly
  */
-public class TreeSearch<A, S> implements SearchForActionsFunction<A, S> {	
-    // function TREE-SEARCH(problem) returns a solution, or failure
-    @Override
-    public List<A> apply(Problem<A, S> problem) {
-        // initialize the frontier using the initial state of the problem
-        Queue<Node<A, S>> frontier = newFrontier(problem.initialState());
-        // loop do
-        while (searchController.isExecuting()) {
-            // if the frontier is empty then return failure
-            if (frontier.isEmpty()) { return searchController.failure(); }
-            // choose a leaf node and remove it from the frontier
-            Node<A, S> node = frontier.remove();
-            // if the node contains a goal state then return the corresponding solution
-            if (searchController.isGoalState(node, problem)) { return searchController.solution(node); }
-            // expand the chosen node, adding the resulting nodes to the frontier
-            for (A action : problem.actions(node.state())) {
-        		frontier.add(nodeFactory.newChildNode(problem, node, action));
-        	}
-        }
-        return searchController.failure();
-    }
-    
-    //
-    // Supporting Code
-    private SearchController<A, S> searchController;
-    private NodeFactory<A, S> nodeFactory;
-    private Supplier<Queue<Node<A, S>>> frontierSupplier;
-    
-    public TreeSearch() {
-    	this(new BasicSearchController<>(), new BasicNodeFactory<>(), LinkedList::new);
-    }
-    
-    public TreeSearch(SearchController<A, S> searchController, NodeFactory<A, S> nodeFactory, Supplier<Queue<Node<A, S>>> frontierSupplier) {
-    	setSearchController(searchController);
-    	setNodeFactory(nodeFactory);
-    	setFrontierSupplier(frontierSupplier);
-    }
-    
-    public void setSearchController(SearchController<A, S> searchController) {
-    	this.searchController = searchController;
-    }
-    
-    public void setNodeFactory(NodeFactory<A, S> nodeFactory) {
-    	this.nodeFactory = nodeFactory;
-    }
-    
-    public void setFrontierSupplier(Supplier<Queue<Node<A, S>>> frontierSupplier) {
-    	this.frontierSupplier = frontierSupplier;
-    }
-    
-    public Queue<Node<A, S>> newFrontier(S initialState) {
-    	Queue<Node<A, S>> frontier = frontierSupplier.get();
-    	frontier.add(nodeFactory.newRootNode(initialState));
-    	return frontier;
-    }
+public class TreeSearch<A, S> implements SearchForActionsFunction<A, S> {
+	// function TREE-SEARCH(problem) returns a solution, or failure
+	@Override
+	public List<A> apply(Problem<A, S> problem) {
+		// initialize the frontier using the initial state of the problem
+		Queue<Node<A, S>> frontier = frontierSupplier.get();
+		frontier.add(nodeFactory.newRootNode(problem.initialState()));
+		// loop do
+		while (searchController.isExecuting()) {
+			// if the frontier is empty then return failure
+			if (frontier.isEmpty()) { return searchController.failure(); }
+			// choose a leaf node and remove it from the frontier
+			Node<A, S> node = frontier.remove();
+			// if the node contains a goal state then return the corresponding solution
+			if (searchController.isGoalState(node, problem)) { return searchController.solution(node);}
+			// expand the chosen node, adding the resulting nodes to the frontier
+			for (A action : problem.actions(node.state())) {
+				frontier.add(nodeFactory.newChildNode(problem, node, action));
+			}
+		}
+		return searchController.failure();
+	}
+
+	//
+	// Supporting Code
+	private SearchController<A, S> searchController;
+	private NodeFactory<A, S> nodeFactory;
+	private Supplier<Queue<Node<A, S>>> frontierSupplier;
+
+	public TreeSearch() {
+		this(new BasicSearchController<>(), new BasicNodeFactory<>(), LinkedList::new);
+	}
+
+	public TreeSearch(SearchController<A, S> searchController, NodeFactory<A, S> nodeFactory,
+			Supplier<Queue<Node<A, S>>> frontierSupplier) {
+		setSearchController(searchController);
+		setNodeFactory(nodeFactory);
+		setFrontierSupplier(frontierSupplier);
+	}
+
+	public void setSearchController(SearchController<A, S> searchController) {
+		this.searchController = searchController;
+	}
+
+	public void setNodeFactory(NodeFactory<A, S> nodeFactory) {
+		this.nodeFactory = nodeFactory;
+	}
+
+	public void setFrontierSupplier(Supplier<Queue<Node<A, S>>> frontierSupplier) {
+		this.frontierSupplier = frontierSupplier;
+	}
 }
--- core/src/main/java/aima/core/search/basic/example/ExampleTreeSearch.java
+++ core/src/main/java/aima/core/search/basic/example/ExampleTreeSearch.java
@@ -14,7 +14,8 @@ import aima.core.search.basic.support.BasicNodeFactory;
 import aima.core.search.basic.support.BasicSearchController;
 
 /**
- * Artificial Intelligence A Modern Approach (4th Edition): Figure ??, page ??.<br>
+ * Artificial Intelligence A Modern Approach (4th Edition): Figure ??, page ??.
+ * <br>
  * <br>
  *
  * <pre>
@@ -31,54 +32,54 @@ import aima.core.search.basic.support.BasicSearchController;
  *
  * @author Ciaran O'Reilly
  */
-public class ExampleTreeSearch<A, S> implements SearchForActionsFunction<A, S> {   
-    // function TREE-SEARCH(problem) returns a solution, or failure
-    @Override
-    public List<A> apply(Problem<A, S> problem) {        
-        // initialize the frontier using the initial state of the problem
-        Queue<Node<A, S>> frontier = newFrontier(problem.initialState());
-        // loop do
-        while (true) {
-            // if the frontier is empty then return failure
-            if (frontier.isEmpty()) { return failure(); }
-            // choose a leaf node and remove it from the frontier
-            Node<A, S> node = frontier.remove();
-            // if the node contains a goal state then return the corresponding solution
-            if (problem.isGoalState(node.state())) { return solution(node); }
-            // expand the chosen node, adding the resulting nodes to the frontier
-            frontier.addAll(expand(node, problem));         
-        }
-    }
-    
-    //
-    // Supporting Code
-    protected NodeFactory<A, S> nodeFactory = new BasicNodeFactory<>();
-    protected SearchController<A, S> searchController = new BasicSearchController<A, S>();
-    
-    public ExampleTreeSearch() {
-    }
-    
-    public Queue<Node<A, S>> newFrontier(S initialState) {
-    	Queue<Node<A, S>> frontier = new LinkedList<>();
-    	frontier.add(nodeFactory.newRootNode(initialState));
-    	return frontier;
-    }
-    
-    public List<A> failure() {
-    	return searchController.failure();
-    }
-    
-    public List<A> solution(Node<A, S> node) {
-    	return searchController.solution(node);
-    }
-    
-    public List<Node<A, S>> expand(Node<A, S> node, Problem<A, S> problem) {
-    	List<Node<A, S>> childNodes = new ArrayList<>();
-    	
-    	for (A action : problem.actions(node.state())) {
-    		childNodes.add(nodeFactory.newChildNode(problem, node, action));
-    	}
-    	
-    	return childNodes;
-    }
+public class ExampleTreeSearch<A, S> implements SearchForActionsFunction<A, S> {
+	// function TREE-SEARCH(problem) returns a solution, or failure
+	@Override
+	public List<A> apply(Problem<A, S> problem) {
+		// initialize the frontier using the initial state of the problem
+		Queue<Node<A, S>> frontier = newFrontier(problem.initialState());
+		// loop do
+		while (true) {
+			// if the frontier is empty then return failure
+			if (frontier.isEmpty()) { return failure(); }
+			// choose a leaf node and remove it from the frontier
+			Node<A, S> node = frontier.remove();
+			// if the node contains a goal state then return the corresponding solution
+			if (problem.isGoalState(node.state())) { return solution(node); }
+			// expand the chosen node, adding the resulting nodes to the frontier
+			frontier.addAll(expand(node, problem));
+		}
+	}
+
+	//
+	// Supporting Code
+	protected NodeFactory<A, S> nodeFactory = new BasicNodeFactory<>();
+	protected SearchController<A, S> searchController = new BasicSearchController<A, S>();
+
+	public ExampleTreeSearch() {
+	}
+
+	public Queue<Node<A, S>> newFrontier(S initialState) {
+		Queue<Node<A, S>> frontier = new LinkedList<>();
+		frontier.add(nodeFactory.newRootNode(initialState));
+		return frontier;
+	}
+
+	public List<A> failure() {
+		return searchController.failure();
+	}
+
+	public List<A> solution(Node<A, S> node) {
+		return searchController.solution(node);
+	}
+
+	public List<Node<A, S>> expand(Node<A, S> node, Problem<A, S> problem) {
+		List<Node<A, S>> childNodes = new ArrayList<>();
+
+		for (A action : problem.actions(node.state())) {
+			childNodes.add(nodeFactory.newChildNode(problem, node, action));
+		}
+
+		return childNodes;
+	}
 }
