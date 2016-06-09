cd2bce8d97cd6318e1f688ded45c3a067b2941e8
Anurag Rai <7anuragrai7@gmail.com>
2016-06-09 18:56:47 +0530

    Added more tests for PLFCEntails

--- core/src/test/java/aima/test/core/unit/logic/propositional/inference/PLFCEntailsTest.java
+++ core/src/test/java/aima/test/core/unit/logic/propositional/inference/PLFCEntailsTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 
 import aima.core.logic.basic.propositional.inference.PLFCEntails;
 import aima.core.logic.basic.propositional.kb.BasicKnowledgeBase;
+import aima.core.logic.basic.common.ParserException;
+
 /**
  * @author Ravi Mohan
  * @author Ciaran O'Reilly
@@ -19,7 +21,29 @@ public class PLFCEntailsTest {
 	public void setUp() {
 		plfce = new PLFCEntails();
 	}
-
+	
+	@Test
+	public void testModusPonens() {
+		BasicKnowledgeBase kb = new BasicKnowledgeBase();
+		kb.tell("A => B");
+		kb.tell("A");
+		
+		Assert.assertEquals(true, plfce.plfcEntails(kb, "B"));
+	}
+	
+	@Test
+	public void testPLFCEntails1() {
+		BasicKnowledgeBase kb = new BasicKnowledgeBase();
+		kb.tell("A & B => C");
+		kb.tell("C & D => E");
+		kb.tell("C & F => G");
+		kb.tell("A");
+		kb.tell("B");
+		kb.tell("D");
+		
+		Assert.assertEquals(true, plfce.plfcEntails(kb, "E"));
+	}
+	
 	@Test
 	public void testAIMAExample() {
 		BasicKnowledgeBase kb = new BasicKnowledgeBase();
@@ -46,5 +70,14 @@ public class PLFCEntailsTest {
 		kb.tell("B");
 		
 		Assert.assertEquals(true, plfce.plfcEntails(kb, "Q"));
+	}
+	
+	@Test(expected=ParserException.class)
+	public void testParserException() {
+		BasicKnowledgeBase kb = new BasicKnowledgeBase();
+		kb.tell("A => B");
+		kb.tell("A");
+		
+		Assert.assertEquals(true, plfce.plfcEntails(kb, "b b"));
 	}
 }
