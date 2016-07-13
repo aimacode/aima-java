9d52c2f0aaaedacde150dfd3e2371cd5afc7c192
Anurag Rai <7anuragrai7@gmail.com>
2016-07-13 22:01:21 +0530

    Add Exception Handler for testing ANTLR parser

---
+++ test/src/test/java/aima/test/unit/antlr/propositional/ExceptionThrowingErrorHandler.java
@@ -0,0 +1,26 @@
+package aima.test.unit.antlr.propositional;
+
+import org.antlr.v4.runtime.DefaultErrorStrategy;
+import org.antlr.v4.runtime.InputMismatchException;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Token;
+
+/**
+ * Error handler which throws exception on any parsing error.
+ */
+public class ExceptionThrowingErrorHandler extends DefaultErrorStrategy {
+	@Override
+	public void recover(Parser recognizer, RecognitionException e) {
+		throw new RuntimeException(e);
+	}
+
+	@Override
+	public Token recoverInline(Parser recognizer) throws RecognitionException {
+		throw new RuntimeException(new InputMismatchException(recognizer));
+	}
+
+	@Override
+	public void sync(Parser recognizer) throws RecognitionException {
+	}
+}
