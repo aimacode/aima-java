89b81a9d88f58ab35aed70e15f3bd795f0ef5117
Anurag Rai <7anuragrai7@gmail.com>
2016-07-17 16:34:55 +0530

    Add Visitor for antlr parser

---
+++ extra/src/main/java/aima/extra/antlr/logic/prop/PropositionalVisitor.java
@@ -0,0 +1,76 @@
+package aima.extra.antlr.logic.prop;
+
+import aima.core.logic.basic.propositional.parsing.ast.ComplexSentence;
+import aima.core.logic.basic.propositional.parsing.ast.Connective;
+import aima.core.logic.basic.propositional.parsing.ast.PropositionSymbol;
+import aima.core.logic.basic.propositional.parsing.ast.Sentence;
+import aima.extra.logic.propositional.parser.PropositionalLogicBaseVisitor;
+import aima.extra.logic.propositional.parser.PropositionalLogicParser;
+import aima.extra.logic.propositional.parser.PropositionalLogicParser.SentenceContext;
+
+public class PropositionalVisitor extends PropositionalLogicBaseVisitor<Sentence> {
+	
+	@Override
+	public Sentence visitParse(PropositionalLogicParser.ParseContext ctx) { 
+		Sentence sentence = null;
+		boolean first = true;
+		for ( SentenceContext s : ctx.sentence() ) {
+			if ( first ) {
+				sentence = visit(s);
+				first = false;
+			}
+			else {
+				sentence = new ComplexSentence(Connective.AND, visit(s), sentence);
+			}
+		}
+		return sentence;
+	}
+	
+	@Override
+	public Sentence visitSentence(PropositionalLogicParser.SentenceContext ctx) {
+		Sentence sentence = null;
+		if ( ctx.bracketedsentence() != null ) {
+			sentence = visitBracketedsentence(ctx.bracketedsentence());
+		}
+		else if ( ctx.atomicsentence() != null) {
+			sentence = visitAtomicsentence(ctx.atomicsentence());
+		}
+		else if ( ctx.op.getText().equals("~") ) {
+			Sentence rightSentence = visit(ctx.right);
+			sentence = new ComplexSentence(Connective.NOT, rightSentence);
+		}
+		else {
+			Sentence leftSentence;
+			Sentence rightSentence;
+			switch ( ctx.op.getText() ) {
+				case "&": 	leftSentence = visit(ctx.left);
+						    rightSentence = visit(ctx.right);
+						    sentence = new ComplexSentence(Connective.AND, leftSentence, rightSentence);
+						    break;
+				case "|":   leftSentence = visit(ctx.left);
+				  		    rightSentence = visit(ctx.right);
+				  		    sentence = new ComplexSentence(Connective.OR, leftSentence, rightSentence);
+				  		    break;
+				case "=>":  leftSentence = visit(ctx.left);
+				  		    rightSentence = visit(ctx.right);
+				  		    sentence = new ComplexSentence(Connective.IMPLICATION, leftSentence, rightSentence);
+				  		    break;
+				case "<=>": leftSentence = visit(ctx.left);
+				  			rightSentence = visit(ctx.right);
+				  			sentence = new ComplexSentence(Connective.BICONDITIONAL, leftSentence, rightSentence);
+				  			break;
+			}
+		}
+		return sentence; 
+	}
+
+	@Override
+	public Sentence visitBracketedsentence(PropositionalLogicParser.BracketedsentenceContext ctx) {
+		return this.visit(ctx.sentence());
+	}
+
+	@Override
+	public Sentence visitAtomicsentence(PropositionalLogicParser.AtomicsentenceContext ctx) {
+		return new PropositionSymbol(ctx.getText());
+	}
+}
