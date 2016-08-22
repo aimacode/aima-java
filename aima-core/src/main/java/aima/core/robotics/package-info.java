e6ae9d5533e5f0eebd9f4e9d9d3bf2fc4ae9038c
janphkre <janphkre@gmx.de>
2016-08-22 21:21:06 +0200

    Updated package-info.

--- aima-core/src/main/java/aima/core/robotics/package-info.java
+++ aima-core/src/main/java/aima/core/robotics/package-info.java
@@ -1,6 +1,5 @@
 /**
- * This package provides an generic apporach to the Monte-Carlo-Localization as defined in the interface {@code IMcl}.<br/>
- * The implementation of that interface can be found in the sub-package {@code impl}.
+ * This package provides an generic apporach to the Monte-Carlo-Localization algorithm.<br/>
  * The sub-packages of {@code impl} provide functionality for a two-dimensional environment.
  */
 package aima.core.robotics;
