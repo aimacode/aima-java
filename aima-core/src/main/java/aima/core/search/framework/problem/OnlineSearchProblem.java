4ed39166d466a7de65f8c155244faca5353554e5
Ruediger Lunde <ruediger.lunde@gmail.com>
2017-06-19 12:13:07 +0200

    OnlineSearchProblem simplified.

--- aima-core/src/main/java/aima/core/search/framework/problem/OnlineSearchProblem.java
+++ aima-core/src/main/java/aima/core/search/framework/problem/OnlineSearchProblem.java
@@ -45,15 +45,4 @@ public interface OnlineSearchProblem<S, A> {
 	 * <code>stateDelta</code> denoted by c(s, a, s').
 	 */
 	double getStepCosts(S state, A action, S stateDelta);
-
-	/**
-	 * Tests whether a node represents an acceptable solution. The default implementation
-	 * delegates the check to the goal test. Other implementations could make use of the additional
-	 * information given by the node (e.g. the sequence of actions leading to the node). A
-	 * solution tester implementation could for example always return false and internally collect
-	 * the paths of all nodes whose state passes the goal test.
-	 */
-	default boolean testSolution(Node<S, A> node) {
-		return testGoal(node.getState());
-	}
 }
