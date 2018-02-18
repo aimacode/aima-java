f3e6bdbac0f7c857774ecda9608cb3c717aaeb66
guptavaibhav18197 <guptavaibhav18197@gmail.com>
2018-02-18 17:49:01 +0530

    Re-implemented RewardFunction.java to incorporate functional interfaces and lambda expressions

--- aima-core/src/main/java/aima/core/learning/reinforcement/agent/PassiveADPAgent.java
+++ aima-core/src/main/java/aima/core/learning/reinforcement/agent/PassiveADPAgent.java
@@ -18,7 +18,7 @@ import aima.core.util.datastructure.Pair;
 /**
  * Artificial Intelligence A Modern Approach (3rd Edition): page 834.<br>
  * <br>
- * 
+ *
  * <pre>
  * function PASSIVE-ADP-AGENT(percept) returns an action
  *   inputs: percept, a percept indicating the current state s' and reward signal r'
@@ -28,7 +28,7 @@ import aima.core.util.datastructure.Pair;
  *               N<sub>sa</sub>, a table of frequencies for state-action pairs, initially zero
  *               N<sub>s'|sa</sub>, a table of outcome frequencies give state-action pairs, initially zero
  *               s, a, the previous state and action, initially null
- *               
+ *
  *   if s' is new then U[s'] <- r'; R[s'] <- r'
  *   if s is not null then
  *        increment N<sub>sa</sub>[s,a] and N<sub>s'|sa</sub>[s',s,a]
@@ -38,19 +38,19 @@ import aima.core.util.datastructure.Pair;
  *   if s'.TERMINAL? then s,a <- null else s,a <- s',&pi;[s']
  *   return a
  * </pre>
- * 
+ *
  * Figure 21.2 A passive reinforcement learning agent based on adaptive dynamic
  * programming. The POLICY-EVALUATION function solves the fixed-policy Bellman
  * equations, as described on page 657.
- * 
+ *
  * @param <S>
  *            the state type.
  * @param <A>
  *            the action type.
- * 
+ *
  * @author Ciaran O'Reilly
  * @author Ravi Mohan
- * 
+ *
  */
 public class PassiveADPAgent<S, A extends Action> extends
 		ReinforcementAgent<S, A> {
@@ -75,7 +75,7 @@ public class PassiveADPAgent<S, A extends Action> extends
 
 	/**
 	 * Constructor.
-	 * 
+	 *
 	 * @param fixedPolicy
 	 *            &pi; a fixed policy.
 	 * @param states
@@ -91,6 +91,8 @@ public class PassiveADPAgent<S, A extends Action> extends
 			S initialState, ActionsFunction<S, A> actionsFunction,
 			PolicyEvaluation<S, A> policyEvaluation) {
 		this.pi.putAll(fixedPolicy);
+		RewardFunction<S> rewardfn = (s) -> R.get(s);
+
 		this.mdp = new MDP<S, A>(states, initialState, actionsFunction,
 				new TransitionProbabilityFunction<S, A>() {
 					public double probability(S sDelta, S s, A a) {
@@ -99,17 +101,13 @@ public class PassiveADPAgent<S, A extends Action> extends
 
 						return null == p ? 0.0 : p.doubleValue();
 					}
-				}, new RewardFunction<S>() {
-					public double reward(S s) {
-						return R.get(s);
-					}
-				});
+				}, rewardfn);
 		this.policyEvaluation = policyEvaluation;
 	}
 
 	/**
 	 * Passive reinforcement learning based on adaptive dynamic programming.
-	 * 
+	 *
 	 * @param percept
 	 *            a percept indicating the current state s' and reward signal
 	 *            r'.
--- aima-core/src/main/java/aima/core/probability/mdp/RewardFunction.java
+++ aima-core/src/main/java/aima/core/probability/mdp/RewardFunction.java
@@ -1,21 +1,23 @@
 package aima.core.probability.mdp;
+import java.util.function.Function;
 
 /**
  * An interface for MDP reward functions.
- * 
- * @param <S>
- *            the state type.
+ *
+ * @param <S> The type used to represent states.
+ *
  * @author Ciaran O'Reilly
  * @author Ravi Mohan
  */
-public interface RewardFunction<S> {
-	
+public interface RewardFunction<S> extends Function<S, Double> {
 	/**
-	 * Get the reward associated with being in state s.
-	 * 
+	 * Extends inbuilt functional interface Function< T, R>.
+	 * having single abstract method
+	 *
+	 * R apply(T)
+	 *
 	 * @param s
 	 *            the state whose award is sought.
 	 * @return the reward associated with being in state s.
 	 */
-	double reward(S s);
 }
