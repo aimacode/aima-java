8fb9fb46df06fe378ad681ac562e52bb482c980a
Ritwik Sharma <ritwik.1204@gmail.com>
2018-03-18 01:37:36 +0530

    Add Environment to AIMA4e

--- core/src/main/java/aima/core/agent/basic/Environment.java
+++ core/src/main/java/aima/core/agent/basic/Environment.java
@@ -9,49 +9,25 @@ import java.util.List;
  * @author Ritwik Sharma
  */
 public interface Environment {
-<<<<<<< HEAD
-	 
-	List<Agent> getAgents();
-
-	/**
-	 * Returns the Agent(s) belonging to this Environment.
-	 */
-	void addAgent(Agent agent);
-
-	/**
-	 * Add an agent to the Environment.
-	 */
-	void removeAgent(Agent agent);
-=======
 	
 	/**
 	 * Returns the Agent(s) belonging to this Environment.
-	 */ 
+	 */
 	List<Agent> getAgents();
 	
 	/**
 	 * Add an agent to the Environment.
 	 */
 	void addAgent(Agent agent);
->>>>>>> 4b0928e2ba050fc4632f60f56368f2216c76b29f
-
+	
 	/**
 	 * Remove an agent from the environment.
 	 */
-<<<<<<< HEAD
-	boolean isDone();
-=======
 	void removeAgent(Agent agent);
->>>>>>> 4b0928e2ba050fc4632f60f56368f2216c76b29f
 
 	/**
 	 * Returns "true" if the Environment is finished with its current task(s).
 	 */
-<<<<<<< HEAD
-=======
 	boolean isDone();
-
-	
->>>>>>> 4b0928e2ba050fc4632f60f56368f2216c76b29f
 
 }
