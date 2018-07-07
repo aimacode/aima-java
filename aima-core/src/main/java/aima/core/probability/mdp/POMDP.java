6b2a62edd7f25615d5ed5942e82edc5faa0e6566
Samagra <samagra14@gmail.com>
2018-07-07 12:14:50 +0530

    Adds plan depths

--- aima-core/src/main/java/aima/core/probability/mdp/POMDP.java
+++ aima-core/src/main/java/aima/core/probability/mdp/POMDP.java
@@ -2,7 +2,11 @@ package aima.core.probability.mdp;
 
 import aima.core.agent.Action;
 
+import java.util.Set;
+
 public interface POMDP<S, A extends Action,E> extends MarkovDecisionProcess<S,A>{
     double getDiscount();
-    double sensorModel(S sDelta, S currentState, A action);
+    double sensorModel(S observedState, S actualState);
+    Set<A> getAllActions();
+
 }
--- aima-core/src/main/java/aima/core/probability/mdp/search/POMDPValueIteration.java
+++ aima-core/src/main/java/aima/core/probability/mdp/search/POMDPValueIteration.java
@@ -22,15 +22,51 @@ public class POMDPValueIteration<S,A extends Action,E> {
         List<Double> utilities = new ArrayList<>();
         for (Object state :
                 pomdp.states()) {
-            utilities.add(0.0);
+            utilities.add(pomdp.reward(state));
         }
         uDash = new HashMap<>();
         uDash.put(new ArrayList<>(),utilities);
         while(maxDifference(u,uDash) < maxError*(1-pomdp.getDiscount())/pomdp.getDiscount()){
             u = new HashMap<>(uDash);
+            uDash = increasePlanDepths(uDash);
             uDash = removeDominatedPlans(uDash);
         }
         return u;
+    }
+
+    private HashMap<List<A>, List<Double>> increasePlanDepths(HashMap<List<A>,
+            List<Double>> uDash) {
+        HashMap<List<A>,List<Double>> result = new HashMap<>();
+        for (A action :
+                this.pomdp.getAllActions()) {
+            for (List<A> plan :
+                    uDash.keySet()) {
+                List<A> newPlan = new ArrayList<>(plan);
+                List<Double> newUtilities = new ArrayList<>();
+                newPlan.add(action);
+                for (S currentState:
+                        this.pomdp.states()) {
+                    double planUtility = 0.0;
+                    for (S actualState :
+                            this.pomdp.states()) {
+                        double tempUtility = 0.0;
+                        for (S observation :
+                             this.pomdp.states()) {
+                            tempUtility+=this.pomdp.sensorModel(observation,
+                                    actualState)*uDash.get(plan).
+                                    get(((ArrayList)this.pomdp.states()).indexOf(actualState));
+                        }
+                        planUtility = tempUtility*this.pomdp.transitionProbability(actualState,
+                                currentState,action);
+                    }
+                    planUtility*= this.pomdp.getDiscount();
+                    planUtility+=this.pomdp.reward(currentState);
+                    newUtilities.add(planUtility);
+                }
+                result.put(newPlan,newUtilities);
+            }
+        }
+        return result;
     }
 
     private HashMap<List<A>, List<Double>> removeDominatedPlans(HashMap<List<A>, List<Double>> uDash) {
