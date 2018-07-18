cf8c2c39210213446fbbeae030eb26236a487dd3
Samagra <samagra14@gmail.com>
2018-07-18 18:08:33 +0530

    Adds comments

--- core/src/main/java/aima/core/search/basic/uninformedsearch/IterativeDeepeningSearch.java
+++ core/src/main/java/aima/core/search/basic/uninformedsearch/IterativeDeepeningSearch.java
@@ -3,13 +3,46 @@ package aima.core.search.basic.uninformedsearch;
 import aima.core.search.api.Node;
 import aima.core.search.api.Problem;
 
+/**
+ * <pre>
+ *
+ /**
+ * Artificial Intelligence A Modern Approach (4th Edition): Figure ??, page ??.
+ * <br>
+ * <br>
+ *
+ * <pre>
+ * function ITERATIVE-DEEPENING-SEARCH(problem) returns a solution, or failure
+ *   for depth = 0 to infinity  do
+ *     result &larr; DEPTH-LIMITED-SEARCH(problem, depth)
+ *     if result != cutoff then return result
+ * </pre>
+ *
+ * Figure ?? The iterative deepening search algorithm, which repeatedly applies
+ * depth-limited search with increasing limits. It terminates when a solution is
+ * found or if the depth-limited search returns failure, meaning that no
+ * solution exists.
+ *
+ * @author Ciaran O'Reilly
+ * @author Ravi Mohan
+ * @author Ruediger Lunde
+ * @author samagra
+ */
 public class IterativeDeepeningSearch<A,S> implements GenericSearchInterface<A,S>{
     DepthLimitedSearch<A,S> depthLimitedSearch = new DepthLimitedSearch<>();
 
+    /**
+     * function ITERATIVE-DEEPENING-SEARCH(problem) returns a solution, or failure
+     * @param problem The search problem
+     * @return The solution if exists else continues to run
+     */
     @Override
     public Node<A, S> search(Problem<A, S> problem) {
+        //for depth = 0 to infinity  do
         for(int depth =0;depth>=0;depth++){
+            //result &larr; DEPTH-LIMITED-SEARCH(problem, depth)
             Node<A,S> result = depthLimitedSearch.search(problem,depth);
+            // if result != cutoff then return result
             if (result!=null)
                 return result;
         }
--- core/src/main/java/aima/core/search/basic/uninformedsearch/UniformCostSearch.java
+++ core/src/main/java/aima/core/search/basic/uninformedsearch/UniformCostSearch.java
@@ -10,8 +10,36 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.PriorityQueue;
 
+/**
+ * <pre>
+ *  function UNIFORM-COST-SEARCH(problem) returns a solution, or failure
+ *  if problem's initial state is a goal then return empty path to initial state
+ *  frontier ← a priority queue ordered by pathCost, with a node for the initial state
+ *  reached ← a table of {state: the best path that reached state}; initially empty
+ *  solution ← failure
+ *  while frontier is not empty and top(frontier) is cheaper than solution do
+ *    parent ← pop(frontier)
+ *    for child in successors(parent) do
+ *      s ← child.state
+ *      if s is not in reached or child is a cheaper path than reached[s] then
+ *        reached[s] ← child
+ *        add child to the frontier
+ *        if child is a goal and is cheaper than solution then
+ *          solution = child
+ *  return solution
+ * </pre>
+ *
+ * Figure 3.11 Uniform-cost search on a graph. Finds optimal
+ * paths for problems with varying step costs.
+ *
+ * @param <A> The generic class representing action.
+ * @param <S> The generic class representing states.
+ *
+ * @author samagra
+ */
 public class UniformCostSearch<A,S> implements GenericSearchInterface<A,S> {
 
+    // frontier ← a priority queue ordered by pathCost, with a node for the initial state
     PriorityQueue<Node<A,S>> frontier = new PriorityQueue<>(new Comparator<Node<A, S>>() {
         @Override
         public int compare(Node<A, S> o1, Node<A, S> o2) {
@@ -19,33 +47,47 @@ public class UniformCostSearch<A,S> implements GenericSearchInterface<A,S> {
         }
     });
 
-    NodeFactory<A,S> nodeFactory = new BasicNodeFactory<>();
+    private NodeFactory<A,S> nodeFactory = new BasicNodeFactory<>();
 
     @Override
     public Node<A, S> search(Problem<A, S> problem) {
+        // if problem's initial state is a goal then return empty path to initial state
         if (problem.isGoalState(problem.initialState())){
             return nodeFactory.newRootNode(problem.initialState());
         }
+        // frontier ← a priority queue ordered by pathCost, with a node for the initial state
         frontier.clear();
         frontier.add(nodeFactory.newRootNode(problem.initialState()));
+        // reached ← a table of {state: the best path that reached state}; initially empty
         HashMap<S, Node<A, S>> reached = new HashMap<>();
+        // solution ← failure
         Node<A,S> solution = null;
+        // while frontier is not empty and top(frontier) is cheaper than solution do
         while (!frontier.isEmpty() &&
                 (solution==null || frontier.peek().pathCost()<solution.pathCost())){
+            // parent ← pop(frontier)
             Node<A,S> parent = frontier.poll();
+            // for child in successors(parent) do
             for (Node<A, S> child :
                     SearchUtils.successors(problem, parent)) {
+                // s ← child.state
                 S s = child.state();
+                // if s is not in reached or child is a cheaper path than reached[s] then
                 if (!reached.containsKey(s) ||
                         child.pathCost()<reached.get(s).pathCost()){
+                    // reached[s] ← child
                     reached.put(s,child);
+                    // add child to the frontier
                     frontier.add(child);
+                    // if child is a goal and is cheaper than solution then
                     if (problem.isGoalState(s) && (solution==null || child.pathCost() <solution.pathCost())){
+                        // solution = child
                         solution = child;
                     }
                 }
             }
         }
+        // return solution
         return solution;
     }
 }
