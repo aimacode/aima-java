ee8ac57e832dbe8428292ee5e95161d19ed3ac02
jesQM <jezuquema@gmail.com>
2020-10-01 17:49:33 +0200

    All heuristics implemented

--- aima-core/src/main/java/aima/core/environment/eightpuzzle/EightPuzzleFunctions.java
+++ aima-core/src/main/java/aima/core/environment/eightpuzzle/EightPuzzleFunctions.java
@@ -14,8 +14,8 @@ import java.util.stream.Stream;
  */
 public class EightPuzzleFunctions {
 
-	public static final EightPuzzleBoard GOAL_STATE = new EightPuzzleBoard(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 }); // ORIGINAL
-//	public static final EightPuzzleBoard GOAL_STATE = new EightPuzzleBoard(new int[] { 1, 2, 3, 8, 0, 4, 7, 6, 5}); // Ejercicio 1
+//	public static final EightPuzzleBoard GOAL_STATE = new EightPuzzleBoard(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 }); // ORIGINAL
+	public static final EightPuzzleBoard GOAL_STATE = new EightPuzzleBoard(new int[] { 1, 2, 3, 8, 0, 4, 7, 6, 5}); // Ejercicio 1
 
 	public static List<Action> getActions(EightPuzzleBoard state) {
 		return Stream.of(EightPuzzleBoard.UP, EightPuzzleBoard.DOWN, EightPuzzleBoard.LEFT, EightPuzzleBoard.RIGHT).
@@ -104,8 +104,8 @@ public class EightPuzzleFunctions {
 		for (int val = 1; val <= 8; val++) {
 			XYLocation locCurr = currState.getLocationOf(val);
 			XYLocation locGoal = GOAL_STATE.getLocationOf(val);
-				result += Math.abs(locGoal.getX() - locCurr.getX()) * Math.pow(2, val);
-				result += Math.abs(locGoal.getY() - locCurr.getY()) * Math.pow(2, val);
+				result += Math.abs(locGoal.getX() - locCurr.getX());
+				result += Math.abs(locGoal.getY() - locCurr.getY());
 			if (result == 2) result += 2 * Math.pow(2, val);
 		}
 		return result;
