95183b312a3d51fb5ecf87b88612903efdd7669e
Judith S <s.ju.sand@gmail.com>
2023-11-16 18:27:42 +0100

    New alldiff(...) constraint.

---
+++ aima-core/src/main/java/aima/core/search/csp/examples/AllDiffConstraint.java
@@ -0,0 +1,42 @@
+package aima.core.search.csp.examples;
+
+import aima.core.search.csp.Assignment;
+import aima.core.search.csp.Constraint;
+import aima.core.search.csp.Variable;
+
+import java.util.*;
+
+/**
+ * Represents a special constraint alldiff(...) where all values have to be different.
+ *
+ * @param <VAR> Variable
+ * @param <VAL> Value, e.g. Integer or String
+ */
+public class AllDiffConstraint<VAR extends Variable, VAL> implements Constraint<VAR, VAL> {
+
+    private final List<VAR> variables;
+
+    public AllDiffConstraint(List<VAR> variables) {
+        this.variables = variables;
+    }
+
+    @Override
+    public List<VAR> getScope() {
+        return this.variables;
+    }
+
+    @Override
+    public boolean isSatisfiedWith(Assignment<VAR, VAL> assignment) {
+        Set<VAL> set = new HashSet<>();
+        int valueCounter = 0;
+
+        for (VAR variable : variables) {
+            VAL value = assignment.getValue(variable);
+            if (value != null) {
+                set.add(value);
+                valueCounter++;
+            }
+        }
+        return set.size() == valueCounter;
+    }
+}
